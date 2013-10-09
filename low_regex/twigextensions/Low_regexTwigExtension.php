dc26186396e6d38ea1448f31d2ce75a84fe70902
Arik Jones <tnypxl@tnypxl-mbp.local>
2013-10-08 20:04:13 -0500

    Add function/filter for matching strings and return true or false

--- low_regex/twigextensions/Low_regexTwigExtension.php
+++ low_regex/twigextensions/Low_regexTwigExtension.php
@@ -21,6 +21,16 @@ class Low_regexTwigExtension extends \Twig_Extension
 		return array('regex' => new \Twig_Function_Method($this, 'regex'));
 	}
 
+	public function getFilters()
+	{
+		return array('regex' => new \Twig_Filter_Method($this, 'match'));
+	}
+
+	public function getFunctions()
+	{
+		return array('regex' => new \Twig_Function_Method($this, 'match'));
+	}
+
 	public function initRuntime(\Twig_Environment $env)
 	{
 		$this->env = $env;
@@ -31,6 +41,16 @@ class Low_regexTwigExtension extends \Twig_Extension
 		if (is_string($var) && strlen($var))
 		{
 			$var = preg_replace($find, $replace, $var);
+		}
+
+		return $var;
+	}
+
+	public function match($var, $find = '', $replace = '')
+	{
+		if (is_string($var) && strlen($var))
+		{
+			$var = preg_match($find, $var);
 		}
 
 		return $var;
